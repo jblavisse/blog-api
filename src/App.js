2256f160ef8303b29e65475f6a0359cca2d758c2
jblavisse <contact@jlavisse.com>
2020-06-23 10:18:38 +0200

    Add timer with toggle button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Timer from "./Timer";
+
+class App extends Component {
+  state = {
+    timerVisible: true
+  }
+
+  toggleTimer = () => {
+    this.setState({
+      timerVisible: !this.state.timerVisible
+    })
+  }
+
+  render() {
+    let timer = this.state.timerVisible ? <Timer/> : "";
+    return (
+      <div className="App">
+        {timer}
+        <button onClick={this.toggleTimer}>Toggle timer</button>
+      </div>
+    );
+  }
+
 }
 
 export default App;
